c480eab4b738ad31e5bd506371dd4443cc8f6013
Marcus Klein <himself@kleini.org>
2019-03-10 19:28:08 +0100

    Revert "Proposal to optionally run HomieNode::loop() also in disconnected state (#490)"
    This reverts commit 9968042d7107cd3b17617aa51e10963277112450.

--- src/Homie/Boot/BootNormal.cpp
+++ src/Homie/Boot/BootNormal.cpp
@@ -111,9 +111,6 @@ void BootNormal::loop() {
     ESP.restart();
   }
 
-  for (HomieNode* iNode : HomieNode::nodes) {
-    if (iNode->runLoopDisconnected ||Interface::get().getMqttClient().connected()) iNode->loop();
-  }
   if (_mqttReconnectTimer.check()) {
     _mqttConnect();
     return;
@@ -181,6 +178,10 @@ void BootNormal::loop() {
   }
 
   Interface::get().loopFunction();
+
+  for (HomieNode* iNode : HomieNode::nodes) {
+    iNode->loop();
+  }
 }
 
 void BootNormal::_prefixMqttTopic() {
